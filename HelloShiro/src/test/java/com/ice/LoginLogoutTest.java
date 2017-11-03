e060ba2e9665ff7aab839b6afe181f58a4a49e3a
liaozhichao <1007790768@qq.com>
2017-11-03 16:12:41 +0800

    添加shiro包和日志包

---
+++ HelloShiro/src/test/java/com/ice/LoginLogoutTest.java
@@ -0,0 +1,15 @@
+package com.ice;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginLogoutTest {
+
+    public  static Logger logger = LoggerFactory.getLogger(LoginLogoutTest.class);
+
+    @Test
+    public void testHelloWorld(){
+        logger.info("Hello World");
+    }
+}
